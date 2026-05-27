2b6c2205a9c4bf5a2ea8e081bbfbe6ebf81db43c
shubham gaikwad <gaikwadruchika25@gmail.com>
2026-05-27 22:09:49 +0530

    dsa3

--- medium/TwoSum.py
+++ medium/TwoSum.py
@@ -1,21 +1,35 @@
-def two_sum(nums, target):
-    hashmap = {}  # value -> index
-
+def two_Sum(nums, target):
+    num_map = {}
     for i in range(len(nums)):
         complement = target - nums[i]
 
-        # Check if complement exists
-        if complement in hashmap:
-            return [hashmap[complement], i]
+        if complement in num_map:
+            return [num_map[complement], i]
 
-        # Store current number
-        hashmap[nums[i]] = i
+        num_map[nums[i]] = i
+nums = [3,3]
+target = 6
+print(two_Sum(nums, target))
 
-    return []  # if no solution
+#### Solution 2  ######
+# def two_sum(nums, target):
+#     n = len(nums)
+#     for i in range(n):
+#         for j in range(i+1,n):
+#             if nums [i] + nums[j] ==  target:
+#                 return[i,j]
+#
+# # Example 1
+# # nums = [2, 7, 11, 15]
+# # target = 9
+# #
+# # #Example 2
+# # nums = [3,2,4]
+# # target = 6
+#
+# # Example 2
+# nums = [3,3]
+# target = 6
+# --print(two_sum(nums, target))
 
 
-# Example usage
-nums = [2, 7, 11, 15]
-target = 9
-
-print(two_sum(nums, target))
