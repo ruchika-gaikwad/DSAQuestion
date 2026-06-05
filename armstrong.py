d8a473a1f21fe6d41724111e545c1813cf0bd1a6
shubham gaikwad <gaikwadruchika25@gmail.com>
2026-06-05 23:56:43 +0530

    dsa11

--- armstrong.py
+++ armstrong.py
@@ -6,3 +6,13 @@ while n:
     s+=d**3
     n//=10
 print('Armstrong' if t==s else 'Not')
+
+
+# n=int(input())
+# t=n
+# s=0
+# while n:
+#     d=n%10
+#     s+=d**3
+#     n//=10
+# print('Armstrong' if t==s else 'Not')
